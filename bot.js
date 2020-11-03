4997bb32bf7d561283bb028acca5eb2dea7fbb3e
ColdDevRealPro <73913897+ColdDevUSSR@users.noreply.github.com>
2020-11-03 16:01:20 -0600

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+ 
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+ 
+
+client.on('message', message => {
+
+    if (message.content === 'ping') {
+
+       message.reply('pong');
+
+       }
+
+});
+
+ 
+
+// THIS  MUST  BE  THIS  WAY
+
+client.login(process.env.sD2gm49t--tdO1bj28CUR9S0UItcnYbH);//BOT_TOKEN is the Client Secret
